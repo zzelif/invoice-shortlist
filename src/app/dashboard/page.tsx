ce4daa1e0295b4820bc21611212313dfe9e4e5d8
dan <dan.lettac@gmail.com>
2025-07-09 18:03:59 +0800

    Add: Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto">
-      <h1 className="text-5xl font-bold">Invoice App</h1>
-      <p>
-        <Button asChild>
-          <Link href="/dashboard">Sign In</Link>
-        </Button>
-      </p>
+      <h1 className="text-5xl font-bold">Dashboard</h1>
     </main>
   );
 }
