cb395b4f668ed5cfdbb95b94b779a9de105ff9de
dan <dan.lettac@gmail.com>
2025-07-09 19:29:44 +0800

    invoice dashboard with the create new route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="no-touch">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
